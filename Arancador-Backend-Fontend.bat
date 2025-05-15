05fe87cfdab6c0c2d74e76afed95422cc52207c4
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-05-14 20:05:37 -0400

    Proyecto base con Vite + React y configuración inicial

---
+++ Arancador-Backend-Fontend.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd c:\proyectos\www\Red-Social-Frontend && npm run dev"
+start cmd.exe /k "cd c:\program Files\mongodb\server\8.0\bin && mongod.exe"
+start cmd.exe /k "cd c:\proyectos\www\api-res-red-social && npm start"
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function App() {
+  return <h1>Proyecto Red Social en React</h1>;
+}
+
+export default App;
